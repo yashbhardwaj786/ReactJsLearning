b4b2e1d8bcc9626d1e388c3d53df2a498ac26db0
yashbhardwaj786 <yashpremsharma@gmail.com>
2022-07-20 17:32:49 +0530

    change title dynamically

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ function App() {
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark Mode has been enabled", "success");
       document.title = "TextUtils - Dark Mode"
-      setInterval(() => {
-        document.title = "TextUtils is Amazing!"
-      }, 2000);
-      setInterval(() => {
-        document.title = "Install TextUtils now"
-      }, 1500);
+      // setInterval(() => {
+      //   document.title = "TextUtils is Amazing!"
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils now"
+      // }, 1500);
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
